e21b8a84fcb33dd9350617c59440fb9ab9ebbcad
Lucas Zluhan <lszluhan@hotmail.com>
2022-01-23 14:47:10 -0300

    first commit

---
+++ dist/main.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const port = process.env.PORT || 8888;
+let app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.use(express_1.default.static(__dirname + `/../public/`));
+app.listen(port, () => console.log(`app rodando na porta ${port}`));
